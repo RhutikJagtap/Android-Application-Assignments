5bf70c7d51fed2d626ee17ae2eadcd069f627e21
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-02-22 16:22:44 +0530

    Update MainActivity.java

--- Option Menu /MainActivity.java
+++ Option Menu /MainActivity.java
@@ -9,7 +9,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class Option extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
